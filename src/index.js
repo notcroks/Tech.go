ff8ae91b5635fb1466beaa4d6471e0e7d26b79e2
notcroks <mikhail.sukharev.m@gmail.com>
2022-04-03 20:25:46 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import 'normalize.css'
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import './styles/main.scss';
 import './index.html';
 
 import $ from 'jquery';
 
 $('.box').html('Hello');
-
-import Swiper, { Navigation, Pagination } from 'swiper';
 
 var swiper = new Swiper(".categorySwiper", {
   slidesPerView: "auto",
